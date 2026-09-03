da8b22a30dd49cf2b0a741e6c5c7bf7400975bcc
Игорь <shestak16@gmail.com>
2026-09-03 20:59:44 +0300

    lesson_1_1 - second commit

--- lesson_1/lesson_1_1.py
+++ lesson_1/lesson_1_1.py
@@ -1,1 +1,33 @@
 print("Hello Python!")
+
+a = 7
+b = 8.34
+str_1 = "легко в ИТ"
+b = a
+a = 8
+print(a, b, str_1)
+
+# a = b = c = 0
+a, b, c = 1, 2, 3
+
+a, b = 9, 10
+a, b = b, a
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(str_1))
+
+# message = "Привет, Игорок"
+# count = 0
+# Count = 0
+
+a = c = 4
+b, d = "Какая-нибудь строка", "Какая-нибудь строка"
+
+print(id(a))
+print(id(b))
+print(id(c))
+print(id(d))
